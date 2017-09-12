b032dd1a6badd7d9e26e1eb59a88d86d577c5799
busmangit <busman.guru@gmail.com>
2017-09-12 12:03:04 -0300

    makefile simplification
    now using single makefile

--- MakeFiles/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lfftw3 -lChi2HD_Cuda -lfftw3f -lcufft -lcudart -llog4cpp -lMagick++ -lMagickWand -lMagickCore -lpthread
-
